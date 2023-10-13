647d07bc36841160e42f99c1de61dfdaa51fd476
Nicole Mellert <nmellert@nd.edu>
2023-10-13 17:03:39 -0400

    Added budget and comments

---
+++ src/Services/BudgetService.js
@@ -0,0 +1,28 @@
+import Parse from 'parse';
+
+// Create a new budget object
+export const createBudget = () => {
+  const Budget = Parse.Object.extend('Budget');
+  const budget = new Budget();
+  return budget.save().then((result) => {
+    return result;
+  });
+};
+
+// Get a budget by its ID
+export const getBudgetById = (id) => {
+  const Budget = Parse.Object.extend('Budget');
+  const query = new Parse.Query(Budget);
+  return query.get(id).then((result) => {
+    return result;
+  });
+};
+
+// READ operation - get all budgets in Parse class Budget
+export const getAllBudgets = () => {
+    const Budget = Parse.Object.extend("Budget");
+    const query = new Parse.Query(Budget);
+    return query.find().then((results) => {
+      return results;
+    });
+  };
